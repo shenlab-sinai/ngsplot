a7e383c35f8feabcdfc95353f139dfe574ec56ee
Li Shen <shenli.sam@gmail.com>
2014-06-09 13:07:47 -0400

    Bumped version number to 2.41

--- bin/ngs.plot.r
+++ bin/ngs.plot.r
@@ -11,7 +11,7 @@
 # library(compiler)
 # enableJIT(3)
 
-ngsplot.version <- '2.10.1'
+ngsplot.version <- '2.41'
 # Program environment variable.
 progpath <- Sys.getenv('NGSPLOT')
 if(progpath == "") {

efe6f0e2c0d7c32143da5fce0e6a0deeda07fbf5
Richard Gunning <rjgunning@gmail.com>
2015-04-08 10:31:33 +0100

    Update ExtractGName.R
    Deal with extra go.list information

--- bin/ExtractGName.R
+++ bin/ExtractGName.R
@@ -1,5 +1,8 @@
+#!/usr/bin/env Rscript
+
 # Extract gene names as ordered by GO algo from ngs.plot output zip file.
 # Input should be the file name of the zip file without .zip suffix.
+library(stringr)
 
 fname <- commandArgs(T)
 
@@ -7,7 +10,7 @@ zip.fname <- paste(fname, 'zip', sep='.')
 heatmap.dat <- file.path(fname, 'heatmap.RData')
 load(unz(zip.fname, heatmap.dat))
 
-gene.list <- strsplit(go.list[[1]], ':')
-gname.list <- sapply(gene.list, function(x) x[1])
+gene.list <- as.data.frame(go.list)
+gname.list <- str_split_fixed(gene.list[,1],':',2)[,1]
 write.table(gname.list, file=paste(fname, 'gname.txt', sep='.'), 
             col.names=F, row.names=F)

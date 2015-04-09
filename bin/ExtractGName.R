2cf1c488e2f9dbfe67a8da45bbdf163dac1ca380
Richard Gunning <rjgunning@gmail.com>
2015-04-09 16:42:01 +0100

    Update ExtractGName.R
    Creates a table containing the gene name and assigned cluster
    Also creates a file for each cluster containing genes in cluster.
    
    Still need to remove stringr dependency and make compatible with non-km algorithms and multiple outputs

--- bin/ExtractGName.R
+++ bin/ExtractGName.R
@@ -11,6 +11,14 @@ heatmap.dat <- file.path(fname, 'heatmap.RData')
 load(unz(zip.fname, heatmap.dat))
 
 gene.list <- as.data.frame(go.list, stringsAsFactors = FALSE)
-gname.list <- str_split_fixed(gene.list[,1],':',2)[,1]
-write.table(gname.list, file=paste(fname, 'gname.txt', sep='.'), 
+gene.list<-as.data.frame(list(str_split_fixed(gene.list[,1],":",2),gene.list[,2]))
+
+write.table(gene.list, file=paste(fname, 'gname.txt', sep='.'), 
             col.names=F, row.names=F)
+
+clusters <- max(gene.list[,3])
+for( i in 1:clusters ){
+  filename <- paste("cluster",i,".txt",sep="")
+  write.table(gene.list[gene.list[,3]==i,1], file=paste(fname, filename, sep='.'),  col.names=F, row.names=F, quote = FALSE)
+}
+

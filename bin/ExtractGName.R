dac88734b4761ced2c9f8139c9f0a930a62d1c43
Richard Gunning <rjgunning@gmail.com>
2015-05-08 16:53:17 +0100

    ExtractGname rewrite
    ExtractGname.R will now read .zip or .RData files
    Will read all lists and output cluster information if it exists

--- bin/ExtractGName.R
+++ bin/ExtractGName.R
@@ -2,23 +2,52 @@
 
 # Extract gene names as ordered by GO algo from ngs.plot output zip file.
 # Input should be the file name of the zip file without .zip suffix.
-library(stringr)
 
-fname <- commandArgs(T)
-
-zip.fname <- paste(fname, 'zip', sep='.')
-heatmap.dat <- file.path(fname, 'heatmap.RData')
-load(unz(zip.fname, heatmap.dat))
-
-gene.list <- as.data.frame(go.list, stringsAsFactors = FALSE)
-gene.list<-as.data.frame(list(str_split_fixed(gene.list[,1],":",2),gene.list[,2]))
-
-write.table(gene.list, file=paste(fname, 'gname.txt', sep='.'), 
-            col.names=F, row.names=F)
-
-clusters <- max(gene.list[,3])
-for( i in 1:clusters ){
-  filename <- paste("cluster",i,".txt",sep="")
-  write.table(gene.list[gene.list[,3]==i,1], file=paste(fname, filename, sep='.'),  col.names=F, row.names=F, quote = FALSE)
+help<-function(){
+	cat("Usage: ExtractGname.R file\nExtract function for ngsplot\nFile can either be the zip file containing the RData file or the RData file directly\n")
+	q(status=1)
 }
 
+fname <- commandArgs(T)
+fname<-"tmp.RData"
+if( length(fname) < 1 || length(fname) > 1){
+	help()
+}
+
+if(grepl(".zip",fname)){
+  root<-strsplit(fname,'.',T)[[1]][1]
+	heatmap.dat <- file.path(root, 'heatmap.RData')
+	load(unz(fname, heatmap.dat))
+} else if (grepl(".RData",fname)) {
+  root<-strsplit(fname,'.',T)[[1]][1]
+  load(fname)
+} else { #assume zip file missing .zip suffix. maintains compatibility
+  heatmap.dat <- file.path(fname, 'heatmap.RData')
+  load(unz(paste(fname,"zip",sep='.'), heatmap.dat))
+}
+
+###Step through each gene list
+for(i in 1:length(go.list[[1]])){
+  if(is.na(go.list[[2]][i])){ #no cluster information
+    gene.list <- data.frame(go.list[[1]][i], stringsAsFactors = FALSE)
+    
+    gname.list <- strsplit(gene.list[,1], ':')
+    gene.list <- data.frame(do.call(rbind, gname.list))
+
+    write.table(gene.list, file=paste(root, i,'gene_name.txt', sep='.'),
+                col.names=F, row.names=F)
+  } else{ # With Clusters
+    gene.list <- data.frame(gene=go.list[[1]][i],cluster=go.list[[2]][i], stringsAsFactors = FALSE)
+    
+    gname.list <- strsplit(gene.list[,1], ':')
+    gene.list <- data.frame(do.call(rbind, gname.list),gene.list[,2])
+    
+    write.table(gene.list, file=paste(root, i,'gene_name.txt', sep='.'),
+                col.names=F, row.names=F)
+    clusters <- max(gene.list[,3])
+    for( j in 1:clusters ){ # Write Clusters
+      filename <- paste("cluster",j,".txt",sep="")
+      write.table(gene.list[gene.list[,3]==j,1], file=paste(root,i, filename, sep='.'),  col.names=F, row.names=F, quote = FALSE)
+    }
+  }
+}
